28c0b0b7f753b7732e6fa39781cd392cc82a84d4
Federico Fruscella <fedefrusc95@gmail.com>
2025-06-11 10:44:02 +0100

    fix: add env var for mongo URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TripsService } from './trip/trip.service';
 @Module({
   imports: [
     TripModule,
-    MongooseModule.forRoot('mongodb://localhost/bizaway'),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/bizaway', {}),
   ],
   controllers: [AppController]
 })
