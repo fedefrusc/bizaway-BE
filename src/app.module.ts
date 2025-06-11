6f2dde569162e73a9b0fd28c1190aeef4bf6d204
Federico Fruscella <fedefrusc95@gmail.com>
2025-06-11 14:22:25 +0100

    fix: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TripModule } from './trip/trip.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { TripsService } from './trip/trip.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     TripModule,
     MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/bizaway', {}),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController]
 })
